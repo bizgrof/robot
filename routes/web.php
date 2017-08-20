8bfc4154439a7121d8fc2f36b5513f993e7e644b
Maga <magbert@mail.ru>
2017-08-20 15:02:19 +0300

    make auth

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('user_info', function () {
 Route::get('product', function () {
     return view('site.product');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
